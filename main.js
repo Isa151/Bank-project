1860c455285f6484b17f0907a0108cbeb7a8d2a5
Baxtzodjon <rasulovbaxtzod@gmailcom>
2024-01-31 23:19:34 +0500

    add modified homework payme project

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { createHeader, reload_table, reload_wallets } from "./modules/ui";
+import { createHeader, reload_table, reload_wallets, storedUserData, storedUserEmail } from "./modules/ui";
 
 // headers 
 let container_header = document.querySelector('.container_header')
@@ -6,5 +6,15 @@ let cont_two = document.querySelector('.container2 .center')
 let tbody = document.querySelector('.container3 tbody');
 
 createHeader(container_header)
-reload_wallets([1,2,4,4,5],cont_two)
-reload_table([1,2,4],tbody)
+reload_wallets([1, 2, 4, 4, 5], cont_two)
+reload_table([1, 2, 4], tbody)
+storedUserData()
+storedUserEmail()
+
+
+// let storedUserData = JSON.parse(localStorage.getItem('user'));
+// document.getElementById('name').innerHTML = 'Добро пожаловать, ' + storedUserData.name + ' ' + storedUserData.surname;
+
+
+// let storedUserEmail = JSON.parse(localStorage.getItem('user'));
+// document.getElementById('email').innerHTML = ' ' + storedUserEmail.email;
