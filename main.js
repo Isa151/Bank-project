fd177ab3ce4418f9ac05791f38259e080da0cf04
tshaxrina <tshaxrinan@gmail.com>
2024-02-03 15:23:41 +0500

    fixed

---
+++ main.js
@@ -0,0 +1,20 @@
+import { createHeader, reload_table, reload_wallets, storedUserData, storedUserEmail } from "./modules/ui";
+
+// headers 
+let container_header = document.querySelector('.container_header')
+let cont_two = document.querySelector('.container2 .center')
+let tbody = document.querySelector('.container3 tbody');
+
+createHeader(container_header)
+reload_wallets([1, 2, 4, 4, 5], cont_two)
+reload_table([1, 2, 4], tbody)
+storedUserData()
+storedUserEmail()
+
+
+// let storedUserData = JSON.parse(localStorage.getItem('user'));
+// document.getElementById('name').innerHTML = 'Добро пожаловать, ' + storedUserData.name + ' ' + storedUserData.surname;
+
+
+// let storedUserEmail = JSON.parse(localStorage.getItem('user'));
+// document.getElementById('email').innerHTML = ' ' + storedUserEmail.email;
