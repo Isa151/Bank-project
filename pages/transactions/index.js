bae36bf656e42b915cb1d7e6c0a0b2fa224b4419
Daler <dsarifkulo021@gmail.com>
2024-02-03 15:40:54 +0500

    fixed:

---
+++ pages/transactions/index.js
@@ -0,0 +1,8 @@
+import { reload_table, createHeader } from "/modules/ui"
+
+
+const container_header = document.querySelector('.container_header')
+const tbody = document.querySelector('.container3 tbody')
+
+createHeader(container_header)
+reload_table([1, 2, 3, 4, 5, 6, 7, 8, 9], tbody)
--- pages/wallets/index.js
+++ pages/wallets/index.js
@@ -1,5 +1,4 @@
-import { reload_wallets, storedUserEmail } from "../../modules/ui"
-import { createHeader } from "/modules/ui.js"
+import { reload_wallets, createHeader } from "/modules/ui"
 
 const container_header = document.querySelector('.container_header')
 let cont_two = document.querySelector('.container2 .center')
@@ -7,7 +6,6 @@ let cont_two = document.querySelector('.container2 .center')
 
 createHeader(container_header)
 reload_wallets([1, 2, 3, 4, , 5, 6, 7], cont_two)
-storedUserEmail()
 
 
 // let storedUserEmail = JSON.parse(localStorage.getItem('user'));
