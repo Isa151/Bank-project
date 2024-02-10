23e7bbab70a9fd494fc09231b3d53c268b34da14
tshaxrina <tshaxrinan@gmail.com>
2024-02-10 14:54:27 +0500

    fixed

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { createHeader, reload_table, reload_wallets } from "./modules/ui";
+import { getData } from "/modules/helpers"
 
 // headers 
 let container_header = document.querySelector('.container_header')
@@ -9,11 +10,19 @@ let gmail = document.querySelector('.management #email')
 gmail.innerHTML = JSON.parse(localStorage.getItem('user')).email;
 
 createHeader(container_header)
-reload_wallets([1, 2, 4, 4, 5], cont_two)
+getData('/wallets?user_id=' + user.id)
+    .then(res => {
+        if(res.status === 200 || res.status === 201) {
+            reload_wallets(res.data, cont_two)
+        }
+    })
+    getData('/wallets?user_id=' + user.id)
+    .then(res => {
+        if(res.status === 200 || res.status === 201) {
+            reload_table(res.data, cont_two)
+        }
+    })
 reload_table([1, 2, 4], tbody)
-
-
-
 
 // let storedUserData = JSON.parse(localStorage.getItem('user'));
 // document.getElementById('name').innerHTML = 'Добро пожаловать, ' + storedUserData.name + ' ' + storedUserData.surname;
