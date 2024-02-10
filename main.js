f898d3ed9802ec52f31ff9b01b9c335976ee1c13
Daler <dsarifkulo021@gmail.com>
2024-02-10 15:13:31 +0500

    fixed

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
-import { createHeader, reload_table, reload_wallets } from "./modules/ui";
-import { getData } from "/modules/helpers"
+import {
+    createHeader,
+    reload_table,
+    reload_wallets
+} from "./modules/ui";
+import {
+    getData
+} from "/modules/helpers"
 
 // headers 
 let container_header = document.querySelector('.container_header')
 let cont_two = document.querySelector('.container2 .center')
 let tbody = document.querySelector('.container3 tbody');
 let gmail = document.querySelector('.management #email')
+let user = JSON.parse(localStorage.getItem('user')) || null
 
-gmail.innerHTML = JSON.parse(localStorage.getItem('user')).email;
+gmail.innerHTML = user.email;
 
 createHeader(container_header)
+
 getData('/wallets?user_id=' + user.id)
     .then(res => {
-        if(res.status === 200 || res.status === 201) {
+        if (res.status === 200 || res.status === 201) {
             reload_wallets(res.data, cont_two)
+            console.log(res);
         }
     })
-    getData('/wallets?user_id=' + user.id)
-    .then(res => {
-        if(res.status === 200 || res.status === 201) {
-            reload_table(res.data, cont_two)
-        }
-    })
-reload_table([1, 2, 4], tbody)
 
 // let storedUserData = JSON.parse(localStorage.getItem('user'));
 // document.getElementById('name').innerHTML = 'Добро пожаловать, ' + storedUserData.name + ' ' + storedUserData.surname;
