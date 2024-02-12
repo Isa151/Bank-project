3ce5f780e24e0c7adeaff6825f3ee152cc0b3034
Baxtzodjon <rasulovbaxtzod@gmailcom>
2024-02-12 23:46:31 +0500

    added credit card homework in js

--- modules/ui.js
+++ modules/ui.js
@@ -59,9 +59,12 @@ export function reload_wallets(arr, place) {
         let creditCardDiv = document.createElement('div');
         let secondSpanDiv = document.createElement('div');
         let firstSpanDiv = document.createElement('div');
+        let link = document.createElement('a');
 
         creditCardDiv.style.background = `linear-gradient(84deg, ${getGradient()} 2.27%, ${getGradient()} 92.26%)`
         creditCardDiv.classList.add(`my_credit_card`);
+        link.classList.add('card_link');
+        link.setAttribute('href', `/pages/card/?id=${item.id}`);
         firstSpanDiv.classList.add('first_span');
         firstSpanDiv.innerHTML = item.name;
         secondSpanDiv.classList.add('second_span');
@@ -69,7 +72,8 @@ export function reload_wallets(arr, place) {
 
         creditCardDiv.append(firstSpanDiv);
         creditCardDiv.append(secondSpanDiv);
-        place.append(creditCardDiv);
+        link.append(creditCardDiv);
+        place.append(link);
     }
 
 }
--- pages/add_wallet/index.js
+++ pages/add_wallet/index.js
@@ -4,32 +4,68 @@ import { getSymbols } from "/modules/helpers";
 const select = document.querySelector('#сurrency')
 const form = document.forms.add_wallet
 const user = JSON.parse(localStorage.getItem('user'))
+let inps = document.querySelectorAll('input')
+
+let patterns = {
+    name: /^[a-z ,.'-]+$/i,
+    currency: /^[a-z ,.'-]+$/i,
+    balance: /^[0-9][A-Za-z0-9 -]*$/
+}
+
+inps.forEach(inp => {
+    let parent = inp.parentElement
+
+    inp.onkeyup = () => {
+        if (patterns[inp.name].test(inp.value)) {
+            parent.classList.remove('error-field')
+        } else {
+            parent.classList.add('error-field')
+        }
+    }
+})
 
 form.onsubmit = (e) => {
     e.preventDefault();
 
     const fm = new FormData(e.target)
-    
+
     const wallet = {
         user_id: user?.id,
     }
 
     fm.forEach((val, key) => wallet[key] = val)
 
-    postData('/wallets', wallet)
-        .then(res => {
-            if(res.status === 200 || res.status === 201) {
-                alert('Success')
-                location.assign('/pages/wallets/')
-            }
-        })
+    let isError = false
+
+    inps.forEach(inp => {
+        let parent = inp.parentElement
+
+        if (inp.value.length === 0 && parent.classList.contains('required') || parent.classList.contains('error-field')) {
+            parent.classList.add('error-field')
+            isError = true
+        } else {
+            parent.classList.remove('error-field')
+        }
+    })
+
+    if (isError) {
+        alert('Error')
+    } else {
+        postData('/wallets', wallet)
+            .then(res => {
+                if (res.status === 200 || res.status === 201) {
+                    alert('Success')
+                    location.assign('/pages/wallets/')
+                }
+            })
+    }
 }
 
 getSymbols()
     .then(res => {
-        for(let key in res) {
+        for (let key in res) {
             let opt = new Option(key + " - " + res[key], key)
-            
+
             select.append(opt)
         }
     })
